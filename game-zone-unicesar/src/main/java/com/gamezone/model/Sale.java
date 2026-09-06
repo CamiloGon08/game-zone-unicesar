1e14d0dae4d4e6e16c928c9279c3d40850d5ad4f
Camilo Andres Gonzalez Garcia <cagonzalezgarcia@unicesar.edu.co>
2026-09-06 00:19:51 -0500

    feat: implement Sale class with total calculation

---
+++ game-zone-unicesar/src/main/java/com/gamezone/model/Sale.java
@@ -0,0 +1,81 @@
+package com.gamezone.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Sale {
+
+    private String id;
+    private LocalDate date;
+    private Seller seller;
+    private Customer customer;
+    private List<Product> products;
+    private double total;
+
+    public Sale(LocalDate date, String id, List<Product> products, Seller seller, Customer customer, double total) {
+        this.date = date;
+        this.id = id;
+        this.products = products;
+        this.seller = seller;
+        this.customer = customer;
+        this.total = total;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public void setCustomer(Customer customer){
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override 
+    public String toString(){
+        return "Sale : "+ id + " Date : "+ date + 
+        ",Customer : " + Customer.getNameC()+ 
+        ",Seller : "+ Seller.getNameS()+
+        ",Products : "+ products.size+ 
+        ", Total : $"+ String.format("%.2f",total);
+    }
+
+}
