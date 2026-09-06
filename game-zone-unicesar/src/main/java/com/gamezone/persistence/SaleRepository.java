837a6bc11528bda9b9e2bd0671cafd2920e398af
Camilo Andres Gonzalez Garcia <cagonzalezgarcia@unicesar.edu.co>
2026-09-06 15:09:34 -0500

    feat:add method to convert sale objects into text format

--- game-zone-unicesar/src/main/java/com/gamezone/persistence/SaleRepository.java
+++ game-zone-unicesar/src/main/java/com/gamezone/persistence/SaleRepository.java
@@ -1,5 +1,21 @@
 package com.gamezone.persistence;
 
-public class SaleRepository {
+import com.gamezone.model.Sale;
 
+public class SaleRepository {
+    private static final String FILE_PATH = "data/sales.txt";
+
+    private String toLine(Sale sale){
+        StringBuilder productNames = new StringBuilder();
+        List<Product> = sale.getProducts();
+        for (int i = 0; i < products.size(); i++) {
+            if(i > 0){
+                productNames.append(";");
+            }
+            productNames.append(products.get(1).getTitle());
+        }
+        return sale.getId()+ "," + sale.getDate()+ ","
+        + sale.getCustomer().getName()+ "," + sale.getSeller().getName()+
+        "," +productNames+ "," +sale.getTotal();
+    }
 }
