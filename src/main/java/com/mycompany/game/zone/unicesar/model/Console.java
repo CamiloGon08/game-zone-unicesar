0bb930cd96774516072e185fe6087fe6bfabc0fd
JesusDominguez <jdaviddominguez@unicesar.edu.co>
2026-09-07 22:49:36 -0500

    feat: add Console class with description implementatio

---
+++ src/main/java/com/mycompany/game/zone/unicesar/model/Console.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.game.zone.unicesar.model;
+
+/**
+ *
+ * @author jxsxs
+ */
+
+
+public class Console extends Product {
+
+    private String brand;
+    private String model;
+    private String generation;
+
+   
+    public Console(String id, String title, double price, int stock,
+                    String brand, String model, String generation) {
+        super(id, title, price, stock);
+        this.brand = brand;
+        this.model = model;
+        this.generation = generation;
+    }
+
+    
+    
+    @Override
+    public String getDescription() {
+        return String.format("%s [Console] - Brand: %s, Model: %s, Generation: %s, Price: %.2f, Stock: %d",
+                getTitle(), brand, model, generation, getPrice(), getStock());
+    }
+
+    
+    public String getBrand() {
+        return brand;
+    }
+
+   
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    
+    public String getModel() {
+        return model;
+    }
+
+    
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+   
+    public String getGeneration() {
+        return generation;
+    }
+
+    
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+}
