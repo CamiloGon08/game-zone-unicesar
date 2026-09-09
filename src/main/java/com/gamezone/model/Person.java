94f4508cb0d53128c3c84e2cf62ad5ddd0a3ac3e
aramis torres <ajuniortorres@unicesar.edu.co>
2026-09-08 19:06:51 -0500

    fix: complete Person and Seller classes with attributes and JavaDoc

--- src/main/java/com/gamezone/model/Person.java
+++ src/main/java/com/gamezone/model/Person.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -8,6 +8,35 @@ package com.gamezone.model;
  *
  * @author I3 DE 10TH
  */
-public class Person {
+public abstract class Person {
+    private String name;
+    private String identification;
+    private String phone;
+    
+    public Person( String name, String identification, String phone){
+    this.name = name;
+    this.identification = identification;
+    this.phone = phone;
+    }
+    public String getName() {
+    return name;
+}
+    public void setName(String name) {
+    this.name = name;
+}
+    public String getIdentification() {
+    return identification;
+}
+
+public void setIdentification(String identification) {
+    this.identification = identification;
+}
+public String getPhone() {
+    return phone;
+}
+
+public void setPhone(String phone) {
+    this.phone = phone;
+}
     
 }
