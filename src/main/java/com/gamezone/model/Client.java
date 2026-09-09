d2964c7d463a34ed2abd482be1fa8dd20ae4499e
aramis torres <ajuniortorres@unicesar.edu.co>
2026-09-08 19:17:28 -0500

    fix: complete Client class with attributes and JavaDoc

--- src/main/java/com/gamezone/model/Client.java
+++ src/main/java/com/gamezone/model/Client.java
@@ -5,9 +5,40 @@
 package com.gamezone.model;
 
 /**
- *
- * @author I3 DE 10TH
+ * Represents a client who purchases products at the store.
  */
-public class Client {
-    
+public class Client extends Person {
+
+    private String email;
+
+    /**
+     * Creates a new Client with the given personal data and contact email.
+     *
+     * @param name           the client's full name
+     * @param identification the client's identification document number
+     * @param phone          the client's contact phone number
+     * @param email          the client's email address
+     */
+    public Client(String name, String identification, String phone, String email) {
+        super(name, identification, phone);
+        this.email = email;
+    }
+
+    /**
+     * Returns the client's email address.
+     *
+     * @return the email address
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the client's email address.
+     *
+     * @param email the new email address
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
